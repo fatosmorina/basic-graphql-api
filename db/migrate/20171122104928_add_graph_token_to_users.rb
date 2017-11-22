2c12ac58585e50710e3a8824d012e52d3fd12479
Fatos Morina <fatosi.morina@gmail.com>
2017-11-22 11:52:26 +0100

    Update user graph token

--- db/migrate/20171122104928_add_graph_token_to_users.rb
+++ db/migrate/20171122104928_add_graph_token_to_users.rb
@@ -3,5 +3,10 @@ class AddGraphTokenToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :graph_token, :string
     add_index :users, :graph_token, unique
 
+    User.find_each do |user|
+      user.update(graph_token: SecureRandom.hex(8))
+    end
+
+    add_column :users, :graph_token, :string, null: false
   end
 end
