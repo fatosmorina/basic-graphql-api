3ea24eb8acdc364448f5334b90d926407665f698
Fatos Morina <fatosi.morina@gmail.com>
2017-11-22 11:31:06 +0100

    Update object from id

--- app/graphql/basic_graphql_api_schema.rb
+++ app/graphql/basic_graphql_api_schema.rb
@@ -5,4 +5,9 @@ BasicGraphqlApiSchema = GraphQL::Schema.define do
   id_from_object ->(obj,type, ctx){
     GraphQL::Schema::UniqueWithinType.encode(type.name, obj.id)
   }
+
+  object_from_id ->(id, ctx){
+    type_name, object_id = GraphQL::Schema::UniqueWithinType.decode(id)
+    type_name.constantize.find(object_id)
+  }
 end
