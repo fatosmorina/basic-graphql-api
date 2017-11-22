80d6e32f10fa3d8c958e0bdca606acc92fa7943e
Fatos Morina <fatosi.morina@gmail.com>
2017-11-22 11:28:14 +0100

    Add id_from_object at the schema

--- app/graphql/basic_graphql_api_schema.rb
+++ app/graphql/basic_graphql_api_schema.rb
@@ -1,4 +1,8 @@
 BasicGraphqlApiSchema = GraphQL::Schema.define do
   mutation(Types::MutationType)
   query(Types::QueryType)
+
+  id_from_object ->(obj,type, ctx){
+    GraphQL::Schema::UniqueWithinType.encode(type.name, obj.id)
+  }
 end
